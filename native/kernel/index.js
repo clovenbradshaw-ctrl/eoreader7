8524a9fe367147ba60d5a77b32890d76e3c7930c
clovenbradshaw-ctrl <clovenbradshaw@gmail.com>
2026-08-23 14:25:51 -0500

    Expose restored population Kind induction

--- native/kernel/index.js
+++ native/kernel/index.js
@@ -1,7 +1,8 @@
 export { MODES, DOMAINS, GRAINS, TERRAIN_BY_DOMAIN, STANCE_BY_MODE, cellOf, algebraAddresses } from "./cube.js";
 export { TERRAINS, terrainOf, createTerrainIndex, indexTerrainEntries, snapshotTerrainState, snapshotTerrainReferents, projectTerrainState, terrainCounts } from "./terrain-state.js";
 export { createEmergentTerrainIndex, indexEmergentTerrainEntries, snapshotEmergentTerrainState, projectEmergentTerrains, mergeTerrainStates } from "./emergent-terrain.js";
-export { kindEvidence, createKindInductionIndex, indexKindEntries, snapshotKindState, kindDiagnostics, projectKinds } from "./kind-induction.js";
+export { kindEvidence, createKindInductionIndex, indexKindEntries, snapshotKindState, kindDiagnostics, kindCandidates, projectKinds } from "./kind-induction.js";
+export { induceEntityParameters, induceEntityKindCandidates, profileJaccard } from "./entity-kind-induction.js";
 export { kindFeatureSignature, kindPrior, deriveKindPriors, rankKindPriorHypotheses, snapshotEntityKindPriorHypotheses, conditionKindProjections } from "./kind-priors.js";
 export { STANCES, stanceOf, createStanceIndex, indexStanceEntries, snapshotStanceState, projectStanceState, stanceCounts } from "./stance-state.js";
 export { receivedGround, eoOperation, deltaFold, applyObservation, applyDelta, reconstruct } from "./fold.js";
