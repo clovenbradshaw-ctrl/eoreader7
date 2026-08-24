d50b9099c04d045060a6b7fbd2a257573b66f52c
clovenbradshaw-ctrl <clovenbradshaw@gmail.com>
2026-08-23 22:06:16 -0500

    Export stance mathematical contracts

--- native/kernel/index.js
+++ native/kernel/index.js
@@ -14,6 +14,7 @@ export { createKindBehaviorAdmissionLedger } from "./kind-behavior-admission.js"
 export { createKindBasinAdmissionLedger } from "./kind-basin-admission.js";
 export { kindFeatureSignature, kindPrior, deriveKindPriors, rankKindPriorHypotheses, snapshotEntityKindPriorHypotheses, conditionKindProjections } from "./kind-priors.js";
 export { STANCES, stanceOf, createStanceIndex, indexStanceEntries, snapshotStanceState, projectStanceState, stanceCounts } from "./stance-state.js";
+export { STANCE_MATHEMATICS, stanceFor, stanceMathematicalSemantics, stanceMathematicalContract, stanceMathematicalAction } from "./stance-math.js";
 export { receivedGround, eoOperation, deltaFold, applyObservation, applyDelta, reconstruct } from "./fold.js";
 export { deriveOrientation } from "./orientation.js";
 export { perceive } from "./perception.js";
